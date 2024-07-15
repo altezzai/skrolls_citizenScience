59046fb9bbfba4ce2ca744c56786aaaee23241bf
farhathulla <farhathulla01@gmail.com>
2024-07-15 17:30:14 +0530

    Home page div change

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import NewPost from "../Components/NewPost/NewPost";
 import Post from "../Components/Post/Post";
+import './CSS/Home.css';
 
 const Home = () => {
   return (
-    <div className="Home">
+    <div className="home">
       <NewPost />
+      <Post />
+      <Post />
+      <Post />
       <Post />
     </div>
   );
