5acc40ccb54f75b16ed04527d57694d70e80e551
farhathulla <farhathulla01@gmail.com>
2024-07-15 17:31:07 +0530

    Post section added

---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import "./Post.css";
+
+import photo from "../../assets/profile.png";
+import image from "../../assets/post-img.png";
+import love from "../../assets/love.svg";
+import msg from "../../assets/message.svg";
+import view from "../../assets/view.svg";
+import send from "../../assets/send.svg";
+import save from "../../assets/save.svg";
+import internet from "../../assets/internet.svg";
+import translate from "../../assets/translate.svg";
+
+const Post = () => {
+  return (
+    <div className="post">
+      <div className="post-profile">
+        <img src={photo} alt="" />
+      </div>
+
+      <div className="post-details">
+        <div className="poster-details">
+          <div className="poster-name">Manu P</div>
+          <div className="posted-date dot">.</div>
+          <div className="posted-date"> April 17</div>
+        </div>
+        <div className="post-content">
+          🚀 Excited to dive into the world of computer science! 💻 Just started
+          my journey as a CS student, and I'm already fascinated by the endless
+          possibilities of coding. From unraveling algorithms to crafting
+          innovative solutions, every day is an adventure! 🌟 Can't wait to see
+          where this path takes me! #CSStudent #CodingAdventure #TechJourney
+          🌐✨:
+        </div>
+        <div className="post-extra">
+          <div className="post-link btn">
+            <img src={internet} alt="" />
+            http://rsg.ms/3426e12
+          </div>
+          <div className="post-link btn">
+            <img src={translate} alt="" />
+            See Translation
+          </div>
+        </div>
+        <div className="post-img">
+          <img src={image} alt="" />
+        </div>
+        <div className="post-icons">
+          <div className="icon-section">
+            <div className="icon">
+              <img src={love} alt="" />
+              200
+            </div>
+
+            <div className="icon">
+              <img src={msg} alt="" />
+              56
+            </div>
+          </div>
+          <div className="icon-section">
+            <div className="icon">
+              <img src={view} alt="" />
+              270
+            </div>
+            <div className="icon">
+              <img src={send} alt="" />
+            </div>
+            <div className="icon">
+              <img src={save} className="save-icon" alt="" />{" "}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
