22af4e20af77a73b4eb82ffc763aeb47b183f58c
farhathulla <farhathulla01@gmail.com>
2024-07-16 15:18:20 +0530

    input changed to div with label

--- src/Components/NewPost/NewPost.jsx
+++ src/Components/NewPost/NewPost.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewPost.css";
 import photo from "../../assets/profile.png";
 import PostButton from "../PostButton/PostButton";
+import AddPost from "../AddPost/AddPost";
 
 const NewPost = () => {
+  const [showAddPost, setShowAddPost] = useState(false);
+
+  const handlePostClick = () => {
+    setShowAddPost(true);
+  };
+
+  const handleCloseClick = () => {
+    setShowAddPost(false);
+  };
+
   return (
     <div className="newpost">
       <div className="post-profile">
         <img src={photo} alt="" />
-        <div className="input-sec">
-          <input type="text" placeholder="What is new. Rafsal?" name="" id="" />
+      </div>
+
+      <div className="input-sec" onClick={handlePostClick}>
+        What is new. Rafsal?
+        <div className="postbtn-container">
+          <PostButton>Add Post!</PostButton>
         </div>
       </div>
 
-      <div className="postbtn-container">
-        <PostButton />
-      </div>
+      <AddPost show={showAddPost} handleClose={handleCloseClick}></AddPost>
     </div>
   );
 };
