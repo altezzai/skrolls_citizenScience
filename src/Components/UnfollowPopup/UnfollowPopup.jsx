91f6a178cd3c99578520c2766dd5f0b046e446b0
farhathulla <farhathulla01@gmail.com>
2024-07-17 18:37:53 +0530

    Popup container content

--- src/Components/UnfollowPopup/UnfollowPopup.jsx
+++ src/Components/UnfollowPopup/UnfollowPopup.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import "./UnfollowPopup.css";
+import photo from "../../assets/profile.png";
 
-const UnfollowPopup = ({closePopup}) => {
+const UnfollowPopup = ({ closePopup, setFollow }) => {
+  const handleUnfollowClick = () => {
+    setFollow(false);
+    closePopup();
+  };
   return (
     <div className="unfollow-popup" onClick={closePopup}>
       <div className="popup-content" onClick={(e) => e.stopPropagation()}>
-        <span className="close-btn" onClick={closePopup}>
-          &times;
-        </span>
-        <h2>Popup Content</h2>
-        <p>This is a pop-up component.</p>
+        <div className="unfollow-detail">
+          <div className="post-profile" style={{ width: "100px" }}>
+            <img src={photo} alt="" style={{ width: "100px" }} />
+          </div>
+          <div className="unfollow-question">Unfollow @ishaque?</div>
+        </div>
+        <div className="decision-buttons">
+          <div className="unfollow-btn" onClick={handleUnfollowClick}>
+            Unfollow
+          </div>
+          <div className="cancel-btn" onClick={closePopup}>
+            Cancel
+          </div>
+        </div>
       </div>
     </div>
   );
