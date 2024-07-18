f1047fef5dde5ae16c9590b06fb8a0ff51782705
Anuragtk117 <anuragtk117@gmail.com>
2024-07-18 12:18:25 +0530

    research activities added

--- src/Components/ResearchActivities/ResearchActivities.jsx
+++ src/Components/ResearchActivities/ResearchActivities.jsx
@@ -5,6 +5,8 @@ import PostButton from "../PostButton/PostButton";
 import download from "../../assets/download.svg";
 import verified from "../../assets/verified.svg";
 import { Contributors } from "../Contributors/Contributors";
+import { ProfilePhoto } from "../Profilephoto/ProfilePhoto";
+import profile from "../../assets/profile.png";
 
 export const ResearchActivities = () => {
   return (
@@ -24,10 +26,28 @@ export const ResearchActivities = () => {
           <span>01 Jan 2022</span>
         </div>
         <Contributors />
-        <div className="source">
-          <img src={verified} />
-          <span>Source:</span>
-          <span>Crossref</span>
+        <div className="expansion">
+          <div className="contribtitle">
+            <span>Contributors</span>
+          </div>
+          <div className="contrib">
+            <div className="imagename">
+              <ProfilePhoto img={profile} size={"24px"} />
+              <a className="pname">A. Majeed</a>
+            </div>
+            <div className="imagename">
+              <ProfilePhoto img={profile} size={"24px"} />
+              <a className="pname">A. Majeed</a>
+            </div>
+            <div className="imagename">
+              <ProfilePhoto img={profile} size={"24px"} />
+              <a className="pname">A. Majeed</a>
+            </div>
+            <div className="imagename">
+              <ProfilePhoto img={profile} size={"24px"} />
+              <a className="pname">A. Majeed</a>
+            </div>
+          </div>
         </div>
       </div>
       <div className="docpart">
@@ -39,6 +59,37 @@ export const ResearchActivities = () => {
           height={"25px"}
           flag={true}
         />
+      </div>
+      <div className="tabdetails">
+        <table className="tableview">
+          <tbody>
+            <tr>
+              <td className="labelname">DOI :</td>
+              <td className="infodata">10.2478/hppj-2022-0001</td>
+            </tr>
+            <tr>
+              <td className="labelname">URL :</td>
+              <td>
+                <a href="" className="urllink">
+                  https://doi.org/10.2478/hppj-2022-0001
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td className="labelname">Added :</td>
+              <td className="infodata">2022-11-18</td>
+            </tr>
+            <tr>
+              <td className="labelname">Last modified :</td>
+              <td className="infodata">2022-11-18</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="source">
+        <img src={verified} />
+        <span>Source:</span>
+        <span>Crossref</span>
       </div>
     </div>
   );
