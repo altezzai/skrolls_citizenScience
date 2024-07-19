0accad0bb72bb412673c90e2bed7bc81bf1844de
farhathulla <farhathulla01@gmail.com>
2024-07-19 19:26:05 +0530

    Link & search icon ref

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import icon from "../../assets/icon.png";
 import search from "../../assets/search.svg";
-import './Header.css';
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
+  const inputRef = useRef(null);
+
+  const handleSearchClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <div className="header">
-      <div className="icon">
-        <img src={icon} alt="" />
-      </div>
+      <Link to="/">
+        <div className="icon">
+          <img src={icon} alt="icon" />
+        </div>
+      </Link>
       <div className="search-box">
-        <img src={search} alt="" />
-        <input type="text" placeholder="Search" name="" id="" />
+        <img
+          src={search}
+          alt="search"
+          onClick={handleSearchClick}
+          style={{ cursor: "pointer" }}
+        />
+        <input type="text" placeholder="Search" ref={inputRef} />
       </div>
     </div>
   );
