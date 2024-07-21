534468cbc8b4ad230c2bd2bced0e1464778022c6
farhathulla <farhathulla01@gmail.com>
2024-07-21 13:02:42 +0530

    hide components from  msgPage

--- src/Components/IdBox/IdBox.jsx
+++ src/Components/IdBox/IdBox.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./IdBox.css";
 import internet from "../../assets/internet.svg";
+import { useLocation } from "react-router-dom";
 
 export const IdBox = () => {
+  const location = useLocation();
+  const inMessagePage = location.pathname.includes("/messages");
+
   return (
-    <div className="idBox">
+    <div className={`${inMessagePage? "hide-idBox": "idBox"}`}>
       <div className="other">
         <span>Other id's</span>
       </div>
--- src/Components/RoyaltyScore/RoyaltyScore.jsx
+++ src/Components/RoyaltyScore/RoyaltyScore.jsx
@@ -1,40 +1,44 @@
 import React from "react";
 import "./RoyaltyScore.css";
 import info from "../../assets/info.svg";
+import { useLocation } from "react-router-dom";
 
 export const RoyaltyScore = () => {
+  const location = useLocation();
+  const inMessagePage = location.pathname.includes("/messages");
+
   return (
-      <div className="royaltyPanel">
-        <ul className="list">
-          <li className="listIcon ">
-            <h1>Royalty Score</h1>
-            <img src={info} className="infologo" />
-          </li>
-          <li className="score leftBorder">
-            <span>384</span>
-            <h5>Score</h5>
-          </li>
-          <li className="citation">
-            <h4>Citations</h4>
-            <div className="line"></div>
-            <h4 className="second">22</h4>
-          </li>
-          <li className="citation">
-            <h4>Peer Reviews</h4>
-            <div className="line"></div>
-            <h4 className="second">07</h4>
-          </li>
-          <li className="citation">
-            <h4>Citizen Score</h4>
-            <div className="line"></div>
-            <h4 className="second">07</h4>
-          </li>
-          <li className="citation">
-            <h4>Download</h4>
-            <div className="line"></div>
-            <h4 className="second">13</h4>
-          </li>
-        </ul>
-      </div>
+    <div className={`royaltyPanel ${inMessagePage ? "hide-royaltyPanel" : ""}`}>
+      <ul className="list">
+        <li className="listIcon ">
+          <h1>Royalty Score</h1>
+          <img src={info} className="infologo" />
+        </li>
+        <li className="score leftBorder">
+          <span>384</span>
+          <h5>Score</h5>
+        </li>
+        <li className="citation">
+          <h4>Citations</h4>
+          <div className="line"></div>
+          <h4 className="second">22</h4>
+        </li>
+        <li className="citation">
+          <h4>Peer Reviews</h4>
+          <div className="line"></div>
+          <h4 className="second">07</h4>
+        </li>
+        <li className="citation">
+          <h4>Citizen Score</h4>
+          <div className="line"></div>
+          <h4 className="second">07</h4>
+        </li>
+        <li className="citation">
+          <h4>Download</h4>
+          <div className="line"></div>
+          <h4 className="second">13</h4>
+        </li>
+      </ul>
+    </div>
   );
 };
