952c7dd2b0425ba6fa49c28da02cf972b7fa4e8b
farhathulla <farhathulla01@gmail.com>
2024-07-31 13:10:46 +0530

    messager list

---
+++ src/Components/UserMsgList/UserMsgList.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import "./UserMsgList.css";
+import UserMsgListItem from "../UserMsgListItem/UserMsgListItem";
+
+const UserMsgList = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleItemClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const users = [
+    { name: "Manuprasad", lastMessage: "hello", time: "10:28 am", count: 10 },
+    {
+      name: "John Doe",
+      lastMessage: "how are you?",
+      time: "9:15 am",
+      count: 5,
+    },
+    {
+      name: "John Doe",
+      lastMessage: "how are you?",
+      time: "9:15 am",
+      count: 5,
+    },
+    {
+      name: "John Doe",
+      lastMessage: "how are you?",
+      time: "9:15 am",
+      count: 5,
+    },
+    { name: "Manuprasad", lastMessage: "hello", time: "10:28 am", count: 10 },
+    { name: "Manuprasad", lastMessage: "hello", time: "10:28 am", count: 10 },
+    { name: "Manuprasad", lastMessage: "hello", time: "10:28 am", count: 10 },
+
+    { name: "Manuprasad", lastMessage: "hello", time: "10:28 am", count: 10 },
+    { name: "Manuprasad", lastMessage: "hello", time: "10:28 am", count: 10 },
+    {
+      name: "John Doe",
+      lastMessage: "how are you?",
+      time: "9:15 am",
+      count: 5,
+    },
+    {
+      name: "John Doe",
+      lastMessage: "how are you?",
+      time: "9:15 am",
+      count: 5,
+    },
+    {
+      name: "John Doe",
+      lastMessage: "how are you?",
+      time: "9:15 am",
+      count: 5,
+    },
+  ];
+
+  return (
+    <div className="user-msg-list">
+      {users.map((user, index) => (
+        <UserMsgListItem
+          key={index}
+          user={user}
+          isActive={activeIndex === index}
+          onClick={() => handleItemClick(index)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default UserMsgList;
