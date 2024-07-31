9b565503419192ec8d58b7b7d6ff7a239716a54c
farhathulla <farhathulla01@gmail.com>
2024-07-31 20:18:26 +0530

    emoji added & msg box in header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,6 +3,9 @@ import icon from "../../assets/icon.png";
 import search from "../../assets/search.svg";
 import skrolls from "../../assets/skrolls.png";
 import { Link, useLocation } from "react-router-dom";
+import { ProfilePhoto } from "../Profilephoto/ProfilePhoto";
+import photo from "../../assets/profile.png";
+import more from "../../assets/vertical_dots.svg";
 import "./Header.css";
 
 const Header = () => {
@@ -35,6 +38,23 @@ const Header = () => {
         />
         <input type="text" placeholder="Search" ref={inputRef} />
       </div>
+
+      {inMessagePage ? (
+        <div className="messagebox-head flex justify-between items-center h-16 px-5 py-2 select-none">
+          <div className="messagebox-user flex items-center gap-3 text-lg font-medium">
+            <ProfilePhoto img={photo} size={"3rem"} />
+            Manuprasad
+          </div>
+          <img
+            className="w-7 cursor-pointer"
+            src={more}
+            alt="More options"
+            draggable="false"
+          />
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
