77740a4c493d32be5b742b71483ff7cd345f358f
farhathulla <farhathulla01@gmail.com>
2024-08-08 15:00:01 +0530

    heart react svg

---
+++ src/assets/component/LikeIcon.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const LikeIcon = ({ className }) => {
+  return (
+    <svg
+      className={className}
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"
+      />
+    </svg>
+  );
+};
