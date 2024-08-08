edc98ce6937edcbbe665563bd7b52cd39ed64003
farhathulla <farhathulla01@gmail.com>
2024-08-08 15:05:44 +0530

    tailwind color location change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,30 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    extend: {
+      colors: {
+        primary: "#f36760",
+        secondary: "#ffeae9",
+        "bg-primary": "#f3f3f3",
+        "bg-secondary": "#ffffff",
+        "bg-muted": "#f5f5f6",
+        "bg-muted-transparent": "#e7e7e740",
+        "bg-transparent": "#000000b8",
+        "bg-transparent-muted": "#00000066",
+        textarea: "#f6f6ff",
+        "border-primary": "#a3a3a3",
+        "border-muted": "#dddddd",
+        "text-primary": "#000000",
+        "text-secondary": "#6c757d",
+        "text-muted": "#c6c6c6",
+        "text-hard": "#3c3c3c",
+        shadow: "rgba(0, 0, 0, 0.05)",
+        grey: "#999",
+        white: "#fff",
+      },
+    },
     extend: {
       colors: {
         primary: "#f36760",
@@ -28,5 +50,6 @@ module.exports = {
     },
   },
   darkMode: "class",
+  plugins: [],
   plugins: [],
 };
