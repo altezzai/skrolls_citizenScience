e03bd2c373f14eb204167cb58f45e00b7e40881f
farhathulla <farhathulla01@gmail.com>
2024-08-08 15:21:00 +0530

    Post page changes

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,95 +1,105 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Post.css";
+import clsx from "clsx";
 
 import photo from "../../assets/profile.png";
 import image from "../../assets/post-img.png";
-import love from "../../assets/love.svg";
 import msg from "../../assets/message.svg";
 import view from "../../assets/view.svg";
 import send from "../../assets/send.svg";
 import save from "../../assets/save.svg";
+import back from "../../assets/previous.svg";
 import internet from "../../assets/internet.svg";
 import translate from "../../assets/translate.svg";
+
 import { ProfilePhoto } from "../Profilephoto/ProfilePhoto";
+import { Link, useLocation } from "react-router-dom";
+import { Heart } from "../ui/Heart";
 
 const Post = () => {
-  const [likeCount, setLikeCount] = useState(200);
-  const [liked, setLiked] = useState(false);
-
-  const handleIconClick = (message) => {
-    console.log(message);
-  };
-
-  const handleLikeClick = () => {
-    if (liked) {
-      setLikeCount(likeCount - 1);
-      setLiked(false);
-      handleIconClick("Unliked");
-    } else {
-      setLikeCount(likeCount + 1);
-      setLiked(true);
-      handleIconClick("Liked");
-    }
-  };
+  const location = useLocation();
+  const inPostPage = location.pathname.includes("/post");
 
   return (
-    <div className="post">
-      <ProfilePhoto img={photo} size={"3rem"} />
+    <>
+      {inPostPage && (
+        <Link
+          to="/"
+          className="back-bgcolor flex gap-2 h-12 items-center border-b-2 rounded-t-2xl text-base px-6 font-medium select-none"
+        >
+          <img src={back} className=" w-4" alt="previous" draggable="false" />
+          Back
+        </Link>
+      )}
 
-      <div className="post-details">
-        <div className="poster-details">
-          <div className="poster-name">Manu P</div>
-          <div className="posted-date dot">.</div>
-          <div className="posted-date"> April 17</div>
-        </div>
-        <div className="post-content">
-          🚀 Excited to dive into the world of computer science! 💻 Just started
-          my journey as a CS student, and I'm already fascinated by the endless
-          possibilities of coding. From unraveling algorithms to crafting
-          innovative solutions, every day is an adventure! 🌟 Can't wait to see
-          where this path takes me! #CSStudent #CodingAdventure #TechJourney
-          🌐✨:
-        </div>
-        <div className="post-extra">
-          <div className="post-link btn">
-            <img src={internet} alt="" />
-            <a href="">http://rsg.ms/3426e12</a>
-          </div>
-          <div className="post-link btn">
-            <img src={translate} alt="" />
-            See Translation
-          </div>
-        </div>
-        <div className="post-img">
-          <img src={image} alt="post image" />
-        </div>
-        <div className="post-icons">
-          <div className="icon-section">
-            <div className="icon" onClick={handleLikeClick}>
-              <img src={love} alt="" />
-              {likeCount}
-            </div>
+      <div
+        className={clsx(
+          "post flex w-full px-5 py-6 rounded-2xl bg-bg-secondary gap-3 ",
+          {
+            "rounded-t-none  mb-2": inPostPage,
+            "mb-5": !inPostPage,
+          }
+        )}
+      >
+        <ProfilePhoto img={photo} size={"3rem"} />
 
-            <div className="icon" onClick={() => handleIconClick("message")}>
-              <img src={msg} alt="" />
-              56
+        <div className="post-details">
+          <div className="poster-details">
+            <div className="poster-name">Manu P</div>
+            <div className="posted-date dot"></div>
+            <div className="posted-date"> April 17</div>
+          </div>
+          <div className="post-content">
+            🚀 Excited to dive into the world of computer science! 💻 Just
+            started my journey as a CS student, and I'm already fascinated by
+            the endless possibilities of coding. From unraveling algorithms to
+            crafting innovative solutions, every day is an adventure! 🌟 Can't
+            wait to see where this path takes me! #CSStudent #CodingAdventure
+            #TechJourney 🌐✨:
+          </div>
+          <div className="post-extra">
+            <div className="post-link btn">
+              <img src={internet} alt="" />
+              <a href="">http://rsg.ms/3426e12</a>
+            </div>
+            <div className="post-link btn">
+              <img src={translate} alt="" />
+              See Translation
             </div>
           </div>
-          <div className="icon-section">
-            <div className="icon">
-              <img src={view} alt="" />
-              270
+          <div className="post-img">
+            <img src={image} alt="post image" />
+          </div>
+          <div className="post-icons">
+            <div className="icon-section">
+              <Heart className={"w-8 h-8 p-1 hover:bg-red-50"} textclr={"text-black"}/>
+
+              <Link to="/post" className="icon">
+                <img src={msg} alt="comment" draggable="false" />
+                56
+              </Link>
             </div>
-            <div className="icon">
-              <img src={send} alt="" />
-            </div>
-            <div className="icon">
-              <img src={save} className="save-icon" alt="" />{" "}
+            <div className="icon-section">
+              <div className="icon">
+                <img src={view} alt="view icon" draggable="false" />
+                270
+              </div>
+              <div className="icon">
+                <img src={send} alt="send message" draggable="false" />
+              </div>
+              <div className="icon">
+                <img
+                  src={save}
+                  className="save-icon"
+                  alt="save message"
+                  draggable="false"
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
