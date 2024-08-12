df4532014a14cbd6dd3a579b81c27f4e3661244d
farhathulla <farhathulla01@gmail.com>
2024-08-12 13:08:12 +0530

    hover effect added

--- src/Components/Profile/ShareProfile.jsx
+++ src/Components/Profile/ShareProfile.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import whatsapp from "../../assets/whatsapp.svg";
 import facebook from "../../assets/facebook_color.svg";
 import mail from "../../assets/mail.svg";
 import twitter from "../../assets/twitter.svg";
 import insta from "../../assets/insta.svg";
+import copy from "copy-to-clipboard";
 
 export const ShareProfile = () => {
+  const [copyText, setCopyText] = useState("http://hereIamYouSeeMeNow");
+
+  const handleCopyText = (e) => {
+    setCopyText(e.target.value);
+  };
+
+  const copyToClipboard = () => {
+    copy(copyText);
+  };
+
   return (
     <div className="bg-bg-secondary flex flex-col rounded-2xl p-3 gap-3">
       <div className="text-sm font-medium select-none">Share</div>
 
-      <div className="flex gap-5 items-end text-xs pb-1 select-none">
+      <div className="flex gap-6 items-end text-xs pb-1 select-none">
         <div className="flex flex-col items-center gap-1">
           <img
             src={facebook}
@@ -66,9 +77,13 @@ export const ShareProfile = () => {
         <input
           type="text"
           className="bg-textarea outline-none w-full pl-3"
-          value="http://hereIamYouSeeMeNow"
+          value={copyText}
+          onChange={handleCopyText}
         />
-        <div className="cursor-pointer bg-primary text-bg-secondary rounded-lg px-5 py-2 select-none">
+        <div
+          className="cursor-pointer bg-primary text-bg-secondary rounded-lg px-5 py-2 select-none hover:bg-red-500 transition-all ease-in delay-75"
+          onClick={copyToClipboard}
+        >
           Copy
         </div>
       </div>
