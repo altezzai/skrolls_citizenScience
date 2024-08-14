b6381d37d0cf3161718f4a6dd8f26e06e5469548
farhathulla <farhathulla01@gmail.com>
2024-08-14 14:46:15 +0530

    message link

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Post.css";
 import clsx from "clsx";
 
 import photo from "../../assets/profile.png";
@@ -15,8 +14,9 @@ import translate from "../../assets/translate.svg";
 import { ProfilePhoto } from "../Profilephoto/ProfilePhoto";
 import { Link, useLocation } from "react-router-dom";
 import { Heart } from "../ui/Heart";
+import { LikedList } from "./LikedList";
 
-const Post = () => {
+const Post = ({ id }) => {
   const location = useLocation();
   const inPostPage = location.pathname.includes("/post");
 
@@ -94,6 +94,7 @@ const Post = () => {
               <Heart
                 className={"w-8 h-8 p-1 hover:bg-red-50"}
                 textclr={"text-black"}
+                id={id}
               />
 
               <Link
@@ -101,12 +102,12 @@ const Post = () => {
                 className="flex items-center justify-center gap-1 cursor-pointer "
               >
                 <div className="rounded-full  hover:bg-red-50 p-1">
-                <img
-                  src={msg}
-                  alt="comment "
-                  className="w-6"
-                  draggable="false"
-                />
+                  <img
+                    src={msg}
+                    alt="comment "
+                    className="w-6"
+                    draggable="false"
+                  />
                 </div>
                 56
               </Link>
