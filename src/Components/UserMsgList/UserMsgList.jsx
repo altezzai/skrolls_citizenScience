bbf90ea5df516ada056934af463f834610fd635a
farhathulla <farhathulla01@gmail.com>
2024-08-15 21:10:58 +0530

    New btn + select none(date&time)

--- src/Components/UserMsgList/UserMsgList.jsx
+++ src/Components/UserMsgList/UserMsgList.jsx
@@ -1,7 +1,9 @@
 import { useState, useRef } from 'react';
 
 import UserMsgListItem from '../UserMsgListItem/UserMsgListItem';
+
 import search from '../../assets/search.svg';
+import plus_icon from '../../assets/plus.svg';
 
 const UserMsgList = () => {
   const inputRef = useRef(null);
@@ -65,19 +67,25 @@ const UserMsgList = () => {
 
   return (
     <div className="min-w-60">
-      <div className="mx-4 my-4 flex gap-3 rounded-3xl bg-textarea px-4 py-2">
-        <img
-          src={search}
-          alt="search"
-          className="w-7 cursor-pointer"
-          onClick={handleSearchClick}
-        />
-        <input
-          type="text"
-          placeholder="Search"
-          className="bg-textarea outline-none"
-          ref={inputRef}
-        />
+      <div className="flex select-none gap-5 px-4 py-4">
+        <div className="flex w-full gap-3 rounded-3xl bg-textarea px-4 py-2">
+          <img
+            src={search}
+            alt="search"
+            className="w-7 cursor-pointer"
+            onClick={handleSearchClick}
+          />
+          <input
+            type="text"
+            placeholder="Search"
+            className="max-w-32 bg-textarea outline-none"
+            ref={inputRef}
+          />
+        </div>
+        <div className="font-normalnt flex cursor-pointer items-center justify-center gap-2 rounded-3xl bg-textarea px-6 py-2">
+          <img src={plus_icon} className="w-5" alt="plus" draggable="false" />
+          New
+        </div>
       </div>
 
       <div
