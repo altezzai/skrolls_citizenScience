257902a4df9d3ab571181743d1d104c579acbc07
farhathulla <farhathulla01@gmail.com>
2024-08-19 19:09:59 +0530

    fixed length

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -12,8 +12,8 @@ export const Login = () => {
 
   return (
     <div className="flex h-screen w-full items-center gap-10 bg-bg-secondary px-20 max-xl:gap-5 max-xl:px-5">
-      <div className="flex w-5/12 flex-col justify-between gap-10 max-lg:w-1/2 max-md:w-full">
-        <div className="mb-5 flex select-none items-center pl-10 text-xl font-bold text-[#324452] max-xl:mb-0 max-lg:text-base">
+      <div className="flex h-4/5 w-5/12 flex-col justify-between gap-10 max-lg:w-1/2 max-md:w-full">
+        <div className="flex select-none items-center pl-10 text-xl font-bold text-[#324452] max-lg:text-base">
           <img
             src={skrolls_logo}
             alt="logo"
@@ -47,7 +47,7 @@ export const Login = () => {
                 />
               </div>
 
-              <div className='flex flex-col gap-1'>
+              <div className="flex flex-col gap-1">
                 <label
                   htmlFor=""
                   className="select-none text-sm font-medium text-text-hard"
@@ -93,16 +93,20 @@ export const Login = () => {
               Login with ORCID
             </div>
           </div>
-          <div className="mt-10 flex w-full justify-center gap-1 text-base text-text-secondary max-xl:mt-0">
-            Need an account?
-            <Link to="/register" className="cursor-pointer select-none font-medium text-text-primary underline">
-              Signup here
-            </Link>
-          </div>
+        </div>
+
+        <div className="flex w-full justify-center gap-1 text-base text-text-secondary">
+          Need an account?
+          <Link
+            to="/register"
+            className="cursor-pointer select-none font-medium text-text-primary underline"
+          >
+            Signup here
+          </Link>
         </div>
       </div>
 
-      <div className="flex h-3/4 w-7/12 select-none items-center justify-center rounded-2xl bg-[#E3EAFF] max-lg:h-2/3 max-lg:w-1/2 max-md:hidden">
+      <div className="flex h-4/5 w-7/12 select-none items-center justify-center rounded-2xl bg-[#E3EAFF] max-lg:h-2/3 max-lg:w-1/2 max-md:hidden">
         <img
           src={login_bg}
           className="h-4/6 w-4/5 max-lg:h-1/2 max-lg:w-3/4"
