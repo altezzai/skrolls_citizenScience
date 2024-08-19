ad84acd50b22c150cd469642f16db8cab8f20fc5
farhathulla <farhathulla01@gmail.com>
2024-08-19 18:58:34 +0530

    Register Page added

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,15 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import login_bg from '../assets/login_bg.png';
 import skrolls_logo from '../assets/skrolls.png';
 import orcid from '../assets/orcid_icon.png';
-import visible from '../assets/visible.svg';
-import invisible from '../assets/invisible.svg';
 import { Link } from 'react-router-dom';
 
-export const Login = () => {
-  const [showPassword, setShowPassword] = useState(false);
-
+export const Register = () => {
   return (
     <div className="flex h-screen w-full items-center gap-10 bg-bg-secondary px-20 max-xl:gap-5 max-xl:px-5">
       <div className="flex w-5/12 flex-col justify-between gap-10 max-lg:w-1/2 max-md:w-full">
@@ -26,9 +22,9 @@ export const Login = () => {
         <div className="flex flex-col items-center gap-10">
           <div className="flex w-8/12 flex-col gap-4 max-lg:w-3/4 max-md:w-11/12">
             <div className="flex select-none flex-col gap-1 max-lg:gap-0">
-              <div className="text-3xl font-bold max-lg:text-lg">Sign in </div>
+              <div className="text-3xl font-bold max-lg:text-lg">Register </div>
               <div className="text-sm font-medium max-lg:text-sm">
-                Welcome back user!
+                Create a new account
               </div>
             </div>
             <form action="" className="flex flex-col gap-2">
@@ -37,38 +33,74 @@ export const Login = () => {
                   htmlFor=""
                   className="select-none text-sm font-medium text-text-hard"
                 >
-                  Email / Username
+                  Full Name
                 </label>
                 <input
+                  id="full_name"
                   type="text"
                   className="rounded-lg border-2 border-border-muted bg-bg-primary px-2 py-1 outline-none"
                   name=""
-                  id=""
                 />
               </div>
 
-              <div className='flex flex-col gap-1'>
+              <div className="flex flex-col gap-1">
+                <label
+                  htmlFor=""
+                  className="select-none text-sm font-medium text-text-hard"
+                >
+                  Username
+                </label>
+                <input
+                  id="username"
+                  type="text"
+                  className="rounded-lg border-2 border-border-muted bg-bg-primary px-2 py-1 outline-none"
+                  name=""
+                />
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <label
+                  htmlFor=""
+                  className="select-none text-sm font-medium text-text-hard"
+                >
+                  Email
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  className="rounded-lg border-2 border-border-muted bg-bg-primary px-2 py-1 outline-none"
+                  name=""
+                />
+              </div>
+
+              <div className="flex flex-col gap-1">
                 <label
                   htmlFor=""
                   className="select-none text-sm font-medium text-text-hard"
                 >
                   Password
                 </label>
-                <div className="flex rounded-lg border-2 border-border-muted bg-bg-primary px-2 py-1">
-                  <input
-                    type={showPassword ? 'text' : 'password'}
-                    name=""
-                    id=""
-                    className="w-full bg-bg-primary outline-none"
-                  />
-                  <img
-                    src={showPassword ? visible : invisible}
-                    className="w-5 cursor-pointer select-none"
-                    alt="visible"
-                    draggable="false"
-                    onClick={() => setShowPassword(!showPassword)}
-                  />
-                </div>
+                <input
+                  id="password"
+                  type="password"
+                  className="rounded-lg border-2 border-border-muted bg-bg-primary px-2 py-1 outline-none"
+                  name=""
+                />
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <label
+                  htmlFor=""
+                  className="select-none text-sm font-medium text-text-hard"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  id="confirm_password"
+                  type="text"
+                  className="rounded-lg border-2 border-border-muted bg-bg-primary px-2 py-1 outline-none"
+                  name=""
+                />
               </div>
 
               <div className="mt-4 flex w-full cursor-pointer select-none justify-center rounded-lg bg-primary py-2 text-bg-secondary transition-all ease-in-out hover:bg-red-500">
@@ -77,26 +109,16 @@ export const Login = () => {
                   type="submit"
                   name=""
                   id=""
-                  value="Login"
+                  value="Register"
                 />
               </div>
             </form>
-
-            <div className="flex select-none justify-center">or</div>
-            <div className="relative flex cursor-pointer select-none items-center justify-center rounded-lg border-2 border-text-primary py-2 text-base font-bold text-text-primary transition-all delay-100 ease-in-out hover:bg-bg-primary">
-              <img
-                src={orcid}
-                alt="ORCID"
-                className="absolute left-[10%] w-7 max-xl:left-3"
-                draggable="false"
-              />
-              Login with ORCID
-            </div>
           </div>
+
           <div className="mt-10 flex w-full justify-center gap-1 text-base text-text-secondary max-xl:mt-0">
-            Need an account?
-            <Link to="/register" className="cursor-pointer select-none font-medium text-text-primary underline">
-              Signup here
+            Already have an account?
+            <Link to="/login" className="cursor-pointer select-none font-medium text-text-primary underline">
+              Login here
             </Link>
           </div>
         </div>
