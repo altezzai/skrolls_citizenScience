44c5c9a81d737345f05ba538b23e875e37fe1f07
farhathulla <farhathulla01@gmail.com>
2024-08-27 15:27:10 +0530

    tab for messages + navpanel add post

--- src/Components/NavPanel/NavPanel.jsx
+++ src/Components/NavPanel/NavPanel.jsx
@@ -6,6 +6,7 @@ import NavPanelItem from './NavPanelItem';
 
 import skrolls_logo from '../../assets/skrolls.png';
 // Icons
+import plus_icon from '../../assets/plus.svg';
 import home_icon from '../../assets/home.svg';
 import messages_icon from '../../assets/messages.svg';
 import notification_icon from '../../assets/notification.svg';
@@ -14,10 +15,21 @@ import communities_icon from '../../assets/community.svg';
 import settings_icon from '../../assets/settings.svg';
 import profile_icon from '../../assets/profile.png';
 import IconBadge from '../IconBadge/IconBadge';
+import { useState } from 'react';
+import AddPost from '../AddPost/AddPost';
 
 export const NavPanel = () => {
   const location = useLocation();
   const inMessagePage = location.pathname.includes('/messages');
+  const [showAddPost, setShowAddPost] = useState(false);
+
+  const handlePostClick = () => {
+    setShowAddPost(true);
+  };
+
+  const handleCloseClick = () => {
+    setShowAddPost(false);
+  };
 
   return (
     <div
@@ -35,61 +47,75 @@ export const NavPanel = () => {
         <img src={skrolls_logo} alt="icon" />
       </Link>
 
-      <div className="rounded-2xl bg-bg-secondary">
-        <NavPanelItem
-          className="rounded-t-2xl"
-          link="/"
-          isSelected={location.pathname === '/'}
-        >
-          <IconBadge>
-            <img src={home_icon} alt="home" />
-          </IconBadge>
-          {!inMessagePage && <span>Home</span>}
-        </NavPanelItem>
+      <div>
+        <div className="flex w-full cursor-pointer select-none justify-center gap-2 rounded-2xl bg-primary py-3 text-sm text-bg-secondary transition-all duration-100 ease-in-out hover:bg-red-500 active:bg-red-800 mb-3"
+        onClick={handlePostClick}>
+          <img
+            src={plus_icon}
+            alt="add post"
+            className="w-5 invert"
+            draggable="false"
+          />
+          {!inMessagePage && 'Create Post'}
+        </div>
+        <AddPost show={showAddPost} handleClose={handleCloseClick}></AddPost>
 
-        <NavPanelItem
-          className=""
-          link="/messages"
-          isSelected={location.pathname === '/messages'}
-        >
-          <IconBadge>
-            <img src={messages_icon} alt="messages" className="px-[1px]" />
-          </IconBadge>
-          {!inMessagePage && <span>Messages</span>}
-        </NavPanelItem>
+        <div className="rounded-2xl bg-bg-secondary">
+          <NavPanelItem
+            className="rounded-t-2xl"
+            link="/"
+            isSelected={location.pathname === '/'}
+          >
+            <IconBadge>
+              <img src={home_icon} alt="home" />
+            </IconBadge>
+            {!inMessagePage && <span>Home</span>}
+          </NavPanelItem>
 
-        <NavPanelItem
-          className=""
-          link="/groups"
-          isSelected={location.pathname === '/groups'}
-        >
-          <IconBadge>
-            <img src={groups_icon} alt="groups" />
-          </IconBadge>
-          {!inMessagePage && <span>Groups</span>}
-        </NavPanelItem>
+          <NavPanelItem
+            className=""
+            link="/messages"
+            isSelected={location.pathname === '/messages'}
+          >
+            <IconBadge>
+              <img src={messages_icon} alt="messages" className="px-[1px]" />
+            </IconBadge>
+            {!inMessagePage && <span>Messages</span>}
+          </NavPanelItem>
 
-        <NavPanelItem
-          className=""
-          link="/communities"
-          isSelected={location.pathname === '/communities'}
-        >
-          <IconBadge>
-            <img src={communities_icon} alt="communities" />
-          </IconBadge>
-          {!inMessagePage && <span>Communities</span>}
-        </NavPanelItem>
+          <NavPanelItem
+            className=""
+            link="/groups"
+            isSelected={location.pathname === '/groups'}
+          >
+            <IconBadge>
+              <img src={groups_icon} alt="groups" />
+            </IconBadge>
+            {!inMessagePage && <span>Groups</span>}
+          </NavPanelItem>
 
-        <NavPanelItem
-          className="rounded-b-2xl"
-          link="/notifications"
-          isSelected={location.pathname === '/notifications'}
-        >
-          <IconBadge className="">
-            <img src={notification_icon} alt="notifications" />
-          </IconBadge>
-          {!inMessagePage && <span>Notification</span>}
-        </NavPanelItem>
+          <NavPanelItem
+            className=""
+            link="/communities"
+            isSelected={location.pathname === '/communities'}
+          >
+            <IconBadge>
+              <img src={communities_icon} alt="communities" />
+            </IconBadge>
+            {!inMessagePage && <span>Communities</span>}
+          </NavPanelItem>
+
+          <NavPanelItem
+            className="rounded-b-2xl"
+            link="/notifications"
+            isSelected={location.pathname === '/notifications'}
+          >
+            <IconBadge className="">
+              <img src={notification_icon} alt="notifications" />
+            </IconBadge>
+            {!inMessagePage && <span>Notification</span>}
+          </NavPanelItem>
+        </div>
       </div>
 
       <div className="rounded-2xl bg-bg-secondary">
